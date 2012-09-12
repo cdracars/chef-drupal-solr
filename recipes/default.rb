d9c61fe94fefc5ba894b2bce450bd2e55fefc03d
Cody Dracars <cdracars@gmail.com>
2012-09-12 15:49:40 -0500

    initail commit

---
+++ recipes/default.rb
@@ -0,0 +1,44 @@
+#
+# Cookbook Name:: Drupal Solr
+# Recipe:: default
+#
+# Copyright 2012, Dracars Designs
+#
+# All rights reserved - Do Not Redistribute
+#
+# To-Do add attributes to abstract values
+
+ package "unzip"
+
+  execute "download-php-library-for-solr" do
+    cwd "/var/www/#{ node[:d7][:directory] }/sites/all/libraries"
+    command "wget http://solr-php-client.googlecode.com/files/SolrPhpClient.r60.2011-05-04.zip"
+    not_if do
+      File.exists?("SolrPhpClient.r60.2011-05-04.zip")
+    end
+  end
+
+  execute "unzip-solr-php-library" do
+    cwd "/var/www/#{ node[:d7][:directory] }/sites/all/libraries"
+    command "unzip SolrPhpClient.r60.2011-05-04.zip && rm -rf SolrPhpClient.r60.2011-05-04.zip"
+    not_if do
+      File.exists?("/var/www/#{ node[:d7][:directory] }/sites/all/libraries/SolrPhpClient")
+    end
+  end
+
+  execute "rename-old-solr-config-files" do
+    cwd "/usr/local/share/apache-solr/example/solr/conf"
+    command "mv schema.xml schema-01.xml; \
+             mv solrconfig.xml solrconfig-01.xml; \
+             mv protwords.txt protwords-01.txt;"
+    not_if do
+      File.exists?("/usr/local/share/apache-solr/example/solr/conf/schema-01.xml")
+    end
+  end
+
+  execute "copy-new-solr-conf-files" do
+    cwd "/usr/local/share/apache-solr/example/solr/conf"
+    command "cp /var/www/#{ node[:d7][:directory] }/sites/#{ node[:d7][:site] }/modules/contrib/search_api_solr/schema.xml .; \
+             cp /var/www/#{ node[:d7][:directory] }/sites/#{ node[:d7][:site] }/modules/contrib/search_api_solr/solrconfig.xml .;"
+    notifies :restart, resources("service[solr]"), :delayed
+  end
